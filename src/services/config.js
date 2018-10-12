7ab76f5220d4d0bf8cf2a640634bd54d7bcb2f0e
machap <paulinhodj10@gmail.com>
2018-10-12 11:07:15 -0300

    deploy 2

--- src/services/config.js
+++ src/services/config.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export const http = axios.create({
     //baseURL: 'http://localhost:8080/api/'
-    baseURL: 'https://apirest-pessoas.herokuapp.com/api'
+    //baseURL: 'https://apirest-pessoas.herokuapp.com/api/'
+    baseURL: '#'
 })
